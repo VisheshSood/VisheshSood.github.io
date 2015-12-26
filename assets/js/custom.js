ce3a771adda41afbc9dcbae37ac54e2084a5ce6a
VisheshSood <omg.itsvishesh@gmail.com>
2015-12-26 20:51:21 +0700

    background

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -43,11 +43,11 @@ VAGAS SLIDESHOW SCRIPTS
 ======================================*/
 $.vegas('slideshow', {
 backgrounds: [
-{ src: 'assets/img/1.jpg', fade: 1000, delay: 9000 },
+{ src: 'assets/img/1.jpg', fade: 1000, delay: 9 },
 ]
 })('overlay', {
 /** SLIDESHOW OVERLAY IMAGE **/
-src: 'assets/js/vegas/overlays/06.png' // THERE ARE TOTAL 01 TO 15 .png IMAGES AT THE PATH GIVEN, WHICH YOU CAN USE HERE
+src: 'assets/js/vegas/overlays/02.png' // THERE ARE TOTAL 01 TO 15 .png IMAGES AT THE PATH GIVEN, WHICH YOU CAN USE HERE
 });
 
 
