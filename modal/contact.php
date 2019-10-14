e963eddd93b06ff470339c76e5857072680cbb58
Vishesh Sood <omg.itsvishesh@gmail.com>
2019-10-14 13:34:02 +0700

    Update!

--- modal/contact.php
+++ modal/contact.php
@@ -1,7 +1,7 @@
 <?php
 
 // Put contacting email here
-$php_main_email = "marketifyteam@gmail.com";
+$php_main_email = "vishesh@innovativeglove.com";
 
 //Fetching Values from URL
 $php_name = $_POST['ajax_name'];
@@ -28,11 +28,11 @@ if (filter_var($php_email, FILTER_VALIDATE_EMAIL)) {
 		
 		$php_template = '<div style="padding:50px;">Hello ' . $php_name . ',<br/>'
 		. 'Thank you for contacting us.<br/><br/>'
-		. '<strong style="color:#f00a77;">Name:</strong>  ' . $php_name . '<br/>'
-		. '<strong style="color:#f00a77;">Email:</strong>  ' . $php_email . '<br/>'
-		. '<strong style="color:#f00a77;">Subject:</strong>  ' . $php_subject1 . '<br/><br/>'
-		. '<strong style="color:#f00a77;">Message:</strong>  ' . $php_message . '<br/><br/>'
-		. 'This is a Contact Confirmation mail.'
+		. '<strong style="color:#00ff00;">Name:</strong>  ' . $php_name . '<br/>'
+		. '<strong style="color:#00ff00;">Email:</strong>  ' . $php_email . '<br/>'
+		. '<strong style="color:#00ff00;">Subject:</strong>  ' . $php_subject1 . '<br/><br/>'
+		. '<strong style="color:#00ff00;">Message:</strong>  ' . $php_message . '<br/><br/>'
+		. 'This is to confirm to you that we have recevied your contact information and details.'
 		. '<br/>'
 		. 'We will contact you as soon as possible .</div>';
 		$php_sendmessage = "<div style=\"background-color:#f5f5f5; color:#333;\">" . $php_template . "</div>";
