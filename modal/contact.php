645dda35291d336b87fad950c32dc3cc89d733a8
Vishesh Sood <vishesh@innovativeglove.com>
2020-04-19 18:09:04 +0700

    dqwdqwd

--- modal/contact.php
+++ modal/contact.php
@@ -1,7 +1,7 @@
 <?php
 
 // Put contacting email here
-$php_main_email = "marketifyteam@gmail.com";
+$php_main_email = "vishesh@innovativeglove.com";
 
 //Fetching Values from URL
 $php_name = $_POST['ajax_name'];
