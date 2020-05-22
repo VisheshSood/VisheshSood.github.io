fc2ea4635c8764d57eb5354a451c33421c618879
Vishesh Sood <vishesh@innovativeglove.com>
2020-05-22 14:37:38 +0700

    Email

--- modal/contact.php
+++ modal/contact.php
@@ -1,7 +1,7 @@
 <?php
 
 // Put contacting email here
-$php_main_email = "vishesh@innovativeglove.com";
+$php_main_email = "vishesh@innovativeglove.com, rsood@innovativeglove.com, avi@innovativeglove.com";
 
 //Fetching Values from URL
 $php_name = $_POST['ajax_name'];
