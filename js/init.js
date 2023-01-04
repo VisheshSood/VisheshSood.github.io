3a1ee919104eb69d7a6deb509fd107631b4bb9b8
dinesh <dinesh.jadhav@ideaqu.com>
2023-01-04 17:39:17 +0530

    removed unwanted code

--- js/init.js
+++ js/init.js
@@ -852,6 +852,7 @@ function request_sample(){
 			jQuery('div.empty_notice').slideDown(500).delay(2000).slideUp(500);
 		}
 		else{
+			jQuery('.returnmessage').hide()
 			// Returns successful data submission message when the entered information is stored in database.
 			jQuery.post("modal/contact.php",{ ajax_name: name, ajax_email: email, ajax_message:message, ajax_subject: subject}, function(data) {
 				
@@ -902,6 +903,7 @@ function glax_tm_contact_form(){
 			jQuery('div.empty_notice').slideDown(500).delay(2000).slideUp(500);
 		}
 		else{
+			jQuery('#send_message').hide()
 			// Returns successful data submission message when the entered information is stored in database.
 			jQuery.post("modal/contact.php",{ ajax_name: name, ajax_email: email, ajax_message:message, ajax_subject: subject}, function(data) {
 				
